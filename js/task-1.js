2bd4e24610fc6d13f950f4b3ec9ee21614a16465
DanyilYatskov <68867876+DanyilYatskov@users.noreply.github.com>
2020-12-16 23:45:43 +0200

    task1-3

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesListRef = [...document.querySelectorAll('.item')];
+const categoriesCount = categoriesListRef.length;
+console.log(`В списке ${categoriesCount} категории`);
+const categoryItemsMapped = categoriesListRef.map(category => {
+  const categoryTitle = category.querySelector('h2');
+  const categoryItems = category.querySelectorAll('li');
+  return { title: categoryTitle.textContent, itemsCount: categoryItems.length };
+});
+categoryItemsMapped.forEach(category => {
+  console.log(
+    `Категория: ${category.title} \r\nКоличество элементов: ${category.itemsCount}`,
+  );
+});
